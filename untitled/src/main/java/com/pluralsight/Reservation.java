91dd5aaf02a5cea4fa6dd9e6b1f1b08ca10486db
hperez29 <perezherbie29@yahoo.com>
2025-05-05 13:41:25 -0400

    HotelWorksheet

---
+++ untitled/src/main/java/com/pluralsight/Reservation.java
@@ -0,0 +1,54 @@
+package com.pluralsight;
+
+public class Reservation {
+    private String roomType;
+    private int numberOfNights;
+    private boolean isWeekend;
+
+    public Reservation(String roomType, int numberOfNights, boolean isWeekend) {
+        this.roomType = roomType;
+        this.numberOfNights = numberOfNights;
+        this.isWeekend = isWeekend;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public double getPrice() {
+        if (roomType.equalsIgnoreCase("king")) {
+            return 139.00;
+        } else if (roomType.equalsIgnoreCase("double")) {
+            return 124.00;
+        }
+        return 0.0;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+
+    public void setIsWeekend(boolean isWeekend) {
+        this.isWeekend = isWeekend;
+    }
+
+    public double getReservationTotal() {
+        double basePrice = getPrice();
+        if (isWeekend) {
+            basePrice *= 1.10;
+        }
+        return basePrice * numberOfNights;
+    }
+}
